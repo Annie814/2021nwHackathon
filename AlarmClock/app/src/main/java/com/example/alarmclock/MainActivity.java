5e2d78e6e1b69dd71c9dc9df27e490243472cd62
madisonhung <madison.hung00@gmail.com>
2021-01-09 16:45:43 -0800

    Maddy's code

--- AlarmClock/app/src/main/java/com/example/alarmclock/MainActivity.java
+++ AlarmClock/app/src/main/java/com/example/alarmclock/MainActivity.java
@@ -1,53 +1,101 @@
 package com.example.alarmclock;
 
-import androidx.appcompat.app.AlertDialog;
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.os.Bundle;
-import android.view.View;
-import android.widget.DatePicker;
-import android.widget.TimePicker;
-import android.widget.TimePicker;
-
 import java.util.Calendar;
-import java.util.GregorianCalendar;
 import java.util.Timer;
 import java.util.TimerTask;
 
-public class MainActivity extends AppCompatActivity {
-    TimePicker alarmTime;
+import android.app.Activity;
+import android.media.Ringtone;
+import android.media.RingtoneManager;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.TimePicker;
 
-        @Override
-        public void onClick(View view) {
 
-            TimePicker alarmTime = findViewById(R.id.time_picker);
+public class MainActivity extends Activity {
+    private TimePicker alarmTime;
+    private TextView currentTime;
+    private Calendar calendar;
+    private String format = "";
 
-            Calendar calendar = new GregorianCalendar(datePicker.getYear(),
-//                    datePicker.getMonth(),
-//                    datePicker.getDayOfMonth(),
-                    alarmTime.getCurrentHour(),
-                    alarmTime.getCurrentMinute());
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-            time = calendar.getTimeInMillis();
-            alertDialog.dismiss();
+        alarmTime = (TimePicker) findViewById(R.id.timePicker1);
+        currentTime = (TextView) findViewById(R.id.textView1);
+        calendar = Calendar.getInstance();
+        final Ringtone r = RingtoneManager.getRingtone(getApplicationContext(),
+                RingtoneManager.getDefaultUri(RingtoneManager.TYPE_RINGTONE));
 
-            Timer t = new Timer();
-            t.scheduleAtFixedRate(new TimerTask() {
-                @Override
-                public void run() {
+        int hour = calendar.get(Calendar.HOUR_OF_DAY);
+        int min = calendar.get(Calendar.MINUTE);
+        showTime(hour, min);
 
+        Timer t = new Timer();
+        t.scheduleAtFixedRate(new TimerTask() {
+            @Override
+            public void run() {
+                if (currentTime.getText().toString().equals(AlarmTime())) {
+                    r.play();
+                } else {
+                    r.stop();
                 }
             }
-
-            alertDialog.setView(dialogView);
-alertDialog.show();
-
+        }, 0, 1000);
+    }
 
 
+    public String AlarmTime() {
+        Integer alarmHours = alarmTime.getCurrentHour();
+        Integer alarmMinutes  = alarmTime.getCurrentMinute();
+        String stringAlarmMinutes;
 
-//    @Override
-//    protected void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_main);
-//    }
+        if (alarmMinutes<10) {
+            stringAlarmMinutes = "0";
+            stringAlarmMinutes = stringAlarmMinutes.concat(alarmMinutes.toString());
+        } else {
+            stringAlarmMinutes = alarmMinutes.toString();
+        }
+
+        String stringAlarmTime = null;
+        if(alarmHours > 12) {
+            alarmHours = alarmHours - 12;
+            stringAlarmTime = alarmHours.toString().concat(":").concat(alarmMinutes.toString()).
+                    concat(" PM");
+        }else{
+            stringAlarmTime = alarmHours.toString().concat(":").concat(alarmMinutes.toString()).
+                    concat(" AM");
+        }
+
+        return stringAlarmTime;
+
+    }
+
+    public void setCurrentTime(View view) {
+        int hour = alarmTime.getCurrentHour();
+        int min = alarmTime.getCurrentMinute();
+        showTime(hour, min);
+    }
+
+    public void showTime(int hour, int min) {
+        if (hour == 0) {
+            hour += 12;
+            format = "AM";
+        } else if (hour == 12) {
+            format = "PM";
+        } else if (hour > 12) {
+            hour -= 12;
+            format = "PM";
+        } else {
+            format = "AM";
+        }
+
+        currentTime.setText(new StringBuilder().append(hour).append(" : ").append(min)
+                .append(" ").append(format));
+    }
+
+
 }
